757ad26c3fefc90496456362bba0322b0062615a
finalezy <13240416021@163.com>
2021-06-17 14:40:20 +0800

    douban success first

--- firstscrapy/first/settings.py
+++ firstscrapy/first/settings.py
@@ -15,6 +15,7 @@ NEWSPIDER_MODULE = 'first.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'test (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- firstscrapy/first/spiders/spider_test.py
+++ firstscrapy/first/spiders/spider_test.py
@@ -1,3 +1,4 @@
+import scrapy
 from scrapy.spiders import Spider
 from first.items import Doubanmovieitem
 
@@ -9,8 +10,13 @@ class DoubanSpider(Spider):
         item = Doubanmovieitem()
         movies =response.xpath('//ol[@class="grid_view"]/li')
         for movie in movies:
-            item['ranking']= movie.xpath('.//div[@class="pic"]/em/text()').extract[0]
-            item['movie_name'] =movie.xpath('.//div[@class="hd"]/span[1]/text()').extract[0]
-            item['score'] = movie.xpath('.//div[@class="star"]/span[@class="rating_num"]/text()').extract[0]
-            item['score_num'] = movie.xpath('.//div[@class="star"]/span/text()').re(r'(\d+)人评价')[0]
+            item['ranking']= movie.xpath(".//div[@class='pic']/em/text()").extract_first()
+            item['movie_name'] =movie.xpath('.//div[@class="hd"]/a/span[1]/text()').extract_first()
+            item['score'] = movie.xpath('.//div[@class="star"]/span[@class="rating_num"]/text()').extract_first()
+            item['score_num'] = movie.xpath('.//div[@class="star"]/span/text()').re(r'(\d+)人评价')
             yield item
+        next_link = response.xpath("//span[@class='next']/a[1]/@href").extract_first()
+        print(next_link)
+        next_url = 'https://movie.douban.com/top250'+next_link
+        print(next_url)
+        yield scrapy.Request(next_url, callback=self.parse)
